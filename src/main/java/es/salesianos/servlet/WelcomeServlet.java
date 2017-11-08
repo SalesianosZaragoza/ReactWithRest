be4ae64e81c7de521c0c4dc259ecfc0f9bab7212
JoTerrance <gorka.zgz@gmail.com>
2017-11-08 08:34:57 +0100

    formulario que pide datos y muestra mensaje de bienvenida

---
+++ src/main/java/es/salesianos/servlet/WelcomeServlet.java
@@ -0,0 +1,35 @@
+package es.salesianos.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doAction(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	}
+
+	private void doAction(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		redirect(req, resp);
+	}
+
+	protected void redirect(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/welcome.jsp.jsp");
+		dispatcher.forward(req, resp);
+	}
+}
