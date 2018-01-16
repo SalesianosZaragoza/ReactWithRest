7fa4812887bca5e1e099c7fc5aef373e68c7d38b
JoTerrance <gorka.zgz@gmail.com>
2018-01-16 09:44:53 +0100

    configuracion de log4j2 sobre springboot

--- src/main/java/es/salesianos/repository/UserRepository.java
+++ src/main/java/es/salesianos/repository/UserRepository.java
@@ -3,6 +3,8 @@ package es.salesianos.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -16,6 +18,8 @@ import es.salesianos.model.User;
 @Component
 public class UserRepository {
 
+	private static Logger log = LogManager.getLogger(UserRepository.class);
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
@@ -23,6 +27,7 @@ public class UserRepository {
 	NamedParameterJdbcTemplate namedJdbcTemplate;
 
 	public void insert(User userFormulario) {
+		log.debug("el log funciona");
 		String sql = "INSERT INTO USER (dni,nombre,apellido)" + "VALUES ( :dni, :nombre, :apellido)";
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue("dni", userFormulario.getDni());
@@ -33,17 +38,19 @@ public class UserRepository {
 
 	public Optional<User> search(User user) {
 		String sql = "SELECT * FROM USER WHERE dni = ?";
+		log.debug("ejecutando la consulta: " + sql);
 		User person = null;
 		try {
 			person = (User) jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper(User.class), user.getDni());
 		} catch (EmptyResultDataAccessException e) {
+			log.error("error", e);
 		}
 		return Optional.ofNullable(person);
 
 	}
 
 	public void update(User user) {
-			String sql = "UPDATE user SET " + "nombre = ?, apellido = ? WHERE dni = ?";
+		String sql = "UPDATE user SET " + "nombre = ?, apellido = ? WHERE dni = ?";
 		jdbcTemplate.update(sql, user.getNombre(), user.getApellido(), user.getDni());
 	}
 
--- src/main/java/es/salesianos/servlet/BaseController.java
+++ src/main/java/es/salesianos/servlet/BaseController.java
@@ -2,14 +2,19 @@ package es.salesianos.servlet;
 
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BaseController {
 
+	private static Logger log = LogManager.getLogger(BaseController.class);
+
 	@GetMapping("/")
 	public String index(Map<String, Object> model) {
+		log.debug("entrando a index");
 		return "index";
 	}
 
