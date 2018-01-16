d6321fb7d75abd676960795a16e11c00fd316a7a
JoTerrance <gorka.zgz@gmail.com>
2018-01-16 09:46:00 +0100

    configurando nuevos profiles en spring h2 y postgres

--- src/main/java/es/salesianos/connection/DatabaseConfig.java
+++ src/main/java/es/salesianos/connection/DatabaseConfig.java
@@ -2,6 +2,7 @@ package es.salesianos.connection;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
@@ -21,10 +22,22 @@ public class DatabaseConfig {
 	 * @return
 	 */
 	@Bean
+	@Profile(value = "h2")
 	public DriverManagerDataSource h2DataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.h2.Driver");
 		dataSource.setUrl("jdbc:h2:file:./src/main/resources/test;INIT=RUNSCRIPT FROM 'classpath:scripts/create.sql'");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	@Bean
+	@Profile(value = "postgres")
+	public DriverManagerDataSource postgresDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgres:localhost:5432'");
 		dataSource.setUsername("sa");
 		dataSource.setPassword("");
 		return dataSource;
